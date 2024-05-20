b8e60a90ac25f2013b2e5bff8621b5aeff7aef76
map0te <mapote@mapote.com>
2024-05-20 14:45:03 -0700

    Initial Chicken (clone A2)

---
+++ game_logic.js
@@ -0,0 +1,31 @@
+export class Game {
+	constructor() {
+		this.game = [];
+		this.level = 0;
+	}
+
+	new_level() {
+		// pull random game out of game list
+	}
+
+	next_level() {
+		// set game to next level
+	}
+
+	reset_level() {
+		// reset level
+	}
+
+	move(move) {
+		// attempt to make given move
+		// return 1 on success
+		return 0
+	}
+
+	ai_sol() {
+		// return list of moves for automatic
+		// solution
+		return 0
+	}
+}
+
